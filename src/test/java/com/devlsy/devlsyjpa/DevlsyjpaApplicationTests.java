465f1f72343c1a58e16f2b44309f3cb584fcdd5e
leesangyeop <devlsy86@mncnext.co.kr>
2022-09-20 13:49:53 +0900

    Initial commit

---
+++ src/test/java/com/devlsy/devlsyjpa/DevlsyjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devlsy.devlsyjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevlsyjpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/devlsy/devlsyjpa/service/MemberServiceTest.java
@@ -0,0 +1,26 @@
+package com.devlsy.devlsyjpa.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+
+class MemberServiceTest {
+
+    @Test
+    void findAllList() {
+    }
+
+    @Test
+    void findMember() {
+    }
+
+    @Test
+    void deleteMember() {
+    }
+
+    @Test
+    void createMember() {
+    }
+}
